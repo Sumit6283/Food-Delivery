a676d8c404c32d0d0bd37a7bd138c0075a82ff3f
Sumit6283 <sumitkr6283@gmail.com>
2024-05-29 00:05:39 +0530

    admin

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,33 +1,41 @@
 import React from 'react'
 import './Add.css'
 import { assets } from '../../assets/assets'
+import { useState } from 'react'
 
 const Add = () => {
+  const [image,setImage] = useState(false)
   return (
     <div className='add'>
       <form className='flex-col' >
-        <div className="add-img-upload">
+        <div className="add-img-upload flex-col">
              <p>Upload Image</p>
              <label htmlFor="image"><img src={assets.upload_area} alt="" /></label>
-             <input type="file" id='image' hidden required />
+             <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image' hidden required />
         </div>
-        <div className="add-product-name">
+        <div className="add-product-name flex-col">
              <p>Product name</p>
              <input type="text" name='name' />
         </div>
-        <div className="product-description">
+        <div className="add-product-description flex-col">
              <p>Product description</p>
               <textarea name="description" rows='6' id=""></textarea>
         </div>
-        <div className="add-category-price">
+        <div className="add-category-price flex-col">
             <div className="add-category">
                 <p>Add Category</p>
                 <select name="cateogory" id="">
-                    <option value="">Pasta</option>
-                    <option value="">Sanwich</option>
+                  <option value="Salad">Salad</option>
+                  <option value="Rolls">Rolls</option>
+                    <option value="Deserts">Deserts</option>
+                    <option value="Sanwich">Sanwich</option>
+                    <option value="Cake">Cake</option>
+                    <option value="Pure Veg">Pure Veg</option>
+                    <option value="Pasta">Pasta</option>
+                    <option value="Noodles">Noodles</option>
                 </select>
             </div>
-            <div className="add-price">
+            <div className="add-price flex-col">
                 <p>Product Price</p>
                 <input name='price' type="Number" />
             </div>
